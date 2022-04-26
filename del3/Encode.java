3887cc751df382ce24ddaad4f50b3f12ad59831e
soren <>
2022-04-26 12:19:08 +0200

    Del 3

---
+++ del3/Encode.java
@@ -0,0 +1,11 @@
+import java.io.File;
+import java.io.FileInputStream;
+
+public class Encode {
+    public static void main( String[] args ){
+       File inFile = new File( args[ 0 ] );
+        
+    }
+    
+    private 
+}
