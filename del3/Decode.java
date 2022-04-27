4bce5d8e27e3e8692765e7a9f9c3206afff7372b
seb <seb@debian.local>
2022-04-27 16:05:45 +0200

    ok

--- del3/Decode.java
+++ del3/Decode.java
@@ -5,32 +5,47 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Decode {
+
+  /*
+  * The main method decodes a compress file from args[0] and saves a decoded file
+  * named args[1].
+  */
   public static void main(String[] args) {
     File compressedFile = new File( args[0] );
     File decodedFile = new File( args[1] );
     int[] frequencies = new int[ 256 ];
 
-    int charCount = 0;
+    // Counter so we know when to stop reading new bits.
+    int byteCount = 0;
 
-
-    try{
+    try {
       BitInputStream input = new BitInputStream( new FileInputStream( compressedFile ) );
-      FileOutputStream output = new FileOutputStream( decodedFile ); 
+      FileOutputStream output = new FileOutputStream( decodedFile );
+
+      // Read frequencies of every byte in original file.
       for ( int i = 0; i < 256; i++ ){
         int n = input.readInt();
         frequencies[ i ] = n;
-        charCount += n;
+        byteCount += n;
       }
+
+      // Make an array of prefix codes for every byte using the huffman tree.
       Node huffmanTree = Huffman.huffman( frequencies );
+
+      // Temp node used to walk through the tree.
       Node tempTree = huffmanTree;
-      while( charCount > 0 ){
-        // read new character
-        while( tempTree.left != null ){ //&& tempTree.right != null
+
+      while( byteCount > 0 ){
+
+        // Walk through tree by reading bits of compressed file.
+        while( tempTree.left != null ) { // && tempTree.right != null (not necessary)
           int bit = input.readBit();
-          tempTree= bit == 0 ? tempTree.left : tempTree.right;
-        } 
+          tempTree = bit == 0 ? tempTree.left : tempTree.right;
+        }
+
+        // Write byte found in leaf of huffman tree.
         output.write( tempTree.bits );
-        charCount--;
+        byteCount--;
         tempTree = huffmanTree;
       }
 
--- del3/Encode.java
+++ del3/Encode.java
@@ -6,80 +6,80 @@ import java.io.IOException;
 
 public class Encode {
 
-    /*
-    * The main method encodes a file from args[0] and saves a compressed file
-    * called args[1].
-    */
-    public static void main( String[] args ){
-        File originalFile = new File( args[ 0 ] );
-        File compressedFile = new File ( args[ 1 ] );
-        int[] frequencies = null;
+  /*
+  * The main method encodes a file from args[0] and saves a compressed file
+  * called args[1].
+  */
+  public static void main( String[] args ){
+    File originalFile = new File( args[ 0 ] );
+    File compressedFile = new File ( args[ 1 ] );
+    int[] frequencies = null;
 
-        try {
-            frequencies = computeFrequencies( originalFile );
+    try {
+      frequencies = computeFrequencies( originalFile );
 
-            // Pointer to root of huffman tree of the byte frequencies.
-            Node huffmanTree = Huffman.huffman(frequencies);
+      // Pointer to root of huffman tree of the byte frequencies.
+      Node huffmanTree = Huffman.huffman(frequencies);
 
-            // Make an array of prefix codes for every byte using the huffman tree.
-            String[] encodingTable = new String[ 256 ];
-            encodeTable( huffmanTree, "", encodingTable );
+      // Make an array of prefix codes for every byte using the huffman tree.
+      String[] encodingTable = new String[ 256 ];
+      encodeTable( huffmanTree, "", encodingTable );
 
-            // Save the frequencies of every byte as the first 1024 bytes of the compressed file.
-            BitOutputStream output = new BitOutputStream( new FileOutputStream( compressedFile ) );
-            for ( int i = 0; i < 256; i++)
-                output.writeInt( frequencies[ i ] );
+      // Save the frequencies of every byte as the first 1024 bytes of the compressed file.
+      BitOutputStream output = new BitOutputStream( new FileOutputStream( compressedFile ) );
+      for ( int i = 0; i < 256; i++)
+        output.writeInt( frequencies[ i ] );
 
-            FileInputStream input = new FileInputStream( originalFile );
-            int read;
+      FileInputStream input = new FileInputStream( originalFile );
+      int read;
 
-            // Read every byte of input file, look up the prefix code of the bytes,
-            // and write the bit string to the compressed file.
-            while ( (read = input.read() ) != -1 ) {
-                String bits = encodingTable[read];
-                for (char bit : bits.toCharArray()) {
-                    int b = bit == '0' ? 0 : 1;
-                    output.writeBit(b);
-                }
-            }
-
-            // Remember to close the output stream to write the buffered bits
-            output.close();
-            input.close();
-
-        } catch ( Exception e ) {
-            e.printStackTrace();
+      // Read every byte of input file, look up the prefix code of the bytes,
+      // and write the bit string to the compressed file.
+      while ( (read = input.read() ) != -1 ) {
+        String bits = encodingTable[read];
+        for (char bit : bits.toCharArray()) {
+          int b = bit == '0' ? 0 : 1;
+          output.writeBit(b);
         }
+      }
+
+      // Remember to close the output stream to write the buffered bits
+      output.close();
+      input.close();
+
+    } catch ( Exception e ) {
+      e.printStackTrace();
     }
+  }
 
-    /*
-    * Computes the frequencies of every byte in file and returns them in an array
-    */
-    private static int[] computeFrequencies( File file ) throws FileNotFoundException, IOException {
-        int[] freqs = new int[ 256 ];
+  /*
+  * Computes the frequencies of every byte in file and returns them in an array
+  */
+  private static int[] computeFrequencies( File file ) throws FileNotFoundException, IOException {
+    int[] freqs = new int[ 256 ];
 
-        FileInputStream input = new FileInputStream( file );
+    FileInputStream input = new FileInputStream( file );
 
-        int read;
-        while( ( read = input.read() ) != -1 )
-            freqs[ read ]++;
+    int read;
+    while( ( read = input.read() ) != -1 )
+      freqs[ read ]++;
 
-        input.close();
+    input.close();
 
-        return freqs;
+    return freqs;
+  }
+
+  /*
+  * Recursively find the prefix code of every leaf in a huffman tree,
+  * and save them in array String[] table, where the index is the numerical
+  * representation of the byte stored in the leaf.
+  */
+  private static void encodeTable( Node x, String currentPath, String[] table ) {
+    if ( x != null ) {
+      encodeTable( x.left, currentPath + "0", table );
+      if ( x.left == null && x.right == null )
+        table[ x.bits ] = currentPath;
+      encodeTable( x.right, currentPath + "1", table );
     }
-
-    /*
-    * Recursively find the prefix code of every leaf in a huffman tree,
-    * and save them in array String[] table, where the index is the numerical
-    * representation of the byte stored in the leaf.
-    */
-    private static void encodeTable( Node x, String currentPath, String[] table ) {
-        if ( x != null ) {
-            encodeTable( x.left, currentPath + "0", table );
-            if ( x.left == null && x.right == null )
-                table[ x.bits ] = currentPath;
-            encodeTable( x.right, currentPath + "1", table );
-        }
-    }
+  }
 }
